257d5961410630c3f44eebfbc017e7880e4eec88
dktest2 <120356313+dktest2@users.noreply.github.com>
2023-09-05 12:55:53 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -58,6 +58,9 @@ async def start(client, message):
             InlineKeyboardButton('JOIN CHATS', url='https://t.me/+qUk9BEId5Lw3MzM1')
             ]]
         reply_markup = InlineKeyboardMarkup(buttons)
+        m=await message.reply_sticker("CAACAgUAAxkBAAEDQb1k5wxpj6FgBlZ7mAe3wRo7vLXxTAACJwEAAsiUZBTiPWKAkUSmmjAE") 
+        await asyncio.sleep(1)
+        await m.delete()
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
